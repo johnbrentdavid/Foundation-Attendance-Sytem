4d92b88cb9b96a866edd3643d716b70bec3cd6db
Eusugii <98152489+Eusugii@users.noreply.github.com>
2023-02-07 22:19:07 +0800

    Add new class that is used for CSS

--- dashboard.php
+++ dashboard.php
@@ -7,23 +7,31 @@
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
   <title>Admin Page</title>
+  <head>
+    <link rel='stylesheet' type="text/css" href='css/dboard.css'>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <div class='header'>
+      <p class='hello'>Hello,</p>
+      <p class='cps'>CPS</p>
+      <p class='dpmt'>Department</p>
+      <img class = img src="img/CPS_Logo.png" alt="CPS Logo">
+      <button class='logout'>Logout</button>
+    </div>
   </head>
   <br><br><br><br>
-  <div style="" ><h1>Dashboard</h1></div>
-  <body>
   
+  <body>
+  <button class="export">Export</button>
   <div class="">
         <table class="">
           <thead> 
             <th change="true">ID</th>  
-            <th change="true">STUDENT NUMBER</th>
-            <th change="true">STUDENT NAME</th>
-            <th change="true">TIME-IN</th>
-            <th change="true">TIME-OUT</th>
+            <th change="true">Student Number</th>
+            <th change="true">Name</th>
+            <th change="true">Time-IN</th>
+            <th change="true">Time-OUT</th>
           </thead>
 
           <?php
